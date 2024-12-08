7aff7d01626c5f7d00e89f204b9593a4a4b41c15
Olaf Kerssemeeckers <kerssemeeckers.olaf@gmail.com>
2024-12-08 15:12:49 +0100

    Add project files.

---
+++ PhotoMadness/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotoMadness
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PhotoMadness/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PhotoMadness
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
